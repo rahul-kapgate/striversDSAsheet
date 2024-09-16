45f18dcf783d2d1d14cf98331733037a24ac1d63
rahul-kapgate <rahulkapgate999@gmail.com>
2024-09-16 10:14:44 +0530

    doc format

--- vector.cpp
+++ vector.cpp
@@ -2,23 +2,24 @@
 #include <vector>
 using namespace std;
 
-int main(){
+int main()
+{
 
-vector <int> unionArr;
+    vector<int> unionArr;
 
-unionArr.push_back(1);
-unionArr.push_back(5);
-unionArr.push_back(7);
-unionArr.push_back(9);
-unionArr.push_back(13);
-unionArr.push_back(5);
+    unionArr.push_back(1);
+    unionArr.push_back(5);
+    unionArr.push_back(7);
+    unionArr.push_back(9);
+    unionArr.push_back(13);
+    unionArr.push_back(5);
 
-int size =  unionArr.size();
+    int size = unionArr.size();
 
-for(int i=0; i<size; i++){
-    cout << unionArr[i] <<endl;
-}
+    for (int i = 0; i < size; i++)
+    {
+        cout << unionArr[i] << endl;
+    }
 
-
-return 0;
+    return 0;
 }
